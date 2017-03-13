41c3780281d129b88527871cecf11c39ba24110c
Achim Rolle <info@achimrolle.de>
2017-03-13 21:31:25 +0100

    add 2-col layout

--- src/App.js
+++ src/App.js
@@ -24,93 +24,98 @@ type Props = {
 const App = ({ config, setConfigAction, activeColor, loadPresetAction }: Props) => (
   <div className="app">
     <h1>CSGO Crosshair Generator</h1>
-    <Toggle
-      name="cl_crosshairusealpha"
-      label="Use Alpha"
-      onClick={setConfigAction}
-      value={config.cl_crosshairusealpha}
-      isActive={config.cl_crosshairusealpha === '1'}
-    />
+    <div className="grid">
+      <div className="col-50">
+        <Toggle
+          name="cl_crosshairusealpha"
+          label="Use Alpha"
+          onClick={setConfigAction}
+          value={config.cl_crosshairusealpha}
+          isActive={config.cl_crosshairusealpha === '1'}
+        />
 
-    <Slider
-      disabled={config.cl_crosshairusealpha === '0'}
-      name="cl_crosshairalpha"
-      label="Alpha"
-      onChange={setConfigAction}
-      min={0}
-      max={255}
-      value={parseInt(config.cl_crosshairalpha, 10)}
-    />
+        <Slider
+          disabled={config.cl_crosshairusealpha === '0'}
+          name="cl_crosshairalpha"
+          label="Alpha"
+          onChange={setConfigAction}
+          min={0}
+          max={255}
+          value={parseInt(config.cl_crosshairalpha, 10)}
+        />
 
-    <Slider
-      name="cl_crosshairthickness"
-      label="Thickness"
-      onChange={setConfigAction}
-      min={0}
-      max={100}
-      step={0.5}
-      value={parseInt(config.cl_crosshairthickness, 10)}
-    />
+        <Slider
+          name="cl_crosshairthickness"
+          label="Thickness"
+          onChange={setConfigAction}
+          min={0}
+          max={100}
+          step={0.5}
+          value={parseInt(config.cl_crosshairthickness, 10)}
+        />
 
-    <Slider
-      name="cl_crosshairsize"
-      label="Size"
-      onChange={setConfigAction}
-      min={0}
-      max={100}
-      value={parseInt(config.cl_crosshairsize, 10)}
-    />
+        <Slider
+          name="cl_crosshairsize"
+          label="Size"
+          onChange={setConfigAction}
+          min={0}
+          max={100}
+          value={parseInt(config.cl_crosshairsize, 10)}
+        />
 
-    <Slider
-      name="cl_fixedcrosshairgap"
-      label="Gap"
-      onChange={setConfigAction}
-      min={-100}
-      max={100}
-      value={parseInt(config.cl_fixedcrosshairgap, 10)}
-    />
+        <Slider
+          name="cl_fixedcrosshairgap"
+          label="Gap"
+          onChange={setConfigAction}
+          min={-100}
+          max={100}
+          value={parseInt(config.cl_fixedcrosshairgap, 10)}
+        />
 
-    <Toggle
-      name="cl_crosshair_outline_draw"
-      label="Draw Outline"
-      onClick={setConfigAction}
-      value={config.cl_crosshair_outline_draw}
-      isActive={config.cl_crosshair_outline_draw === '1'}
-    />
+        <Toggle
+          name="cl_crosshair_outline_draw"
+          label="Draw Outline"
+          onClick={setConfigAction}
+          value={config.cl_crosshair_outline_draw}
+          isActive={config.cl_crosshair_outline_draw === '1'}
+        />
 
-    <Slider
-      disabled={config.cl_crosshair_outline_draw === '0'}
-      name="cl_crosshair_outline"
-      label="Outline"
-      onChange={setConfigAction}
-      min={0}
-      max={3}
-      dots
-      value={parseInt(config.cl_crosshair_outline, 10)}
-    />
+        <Slider
+          disabled={config.cl_crosshair_outline_draw === '0'}
+          name="cl_crosshair_outline"
+          label="Outline"
+          onChange={setConfigAction}
+          min={0}
+          max={3}
+          dots
+          value={parseInt(config.cl_crosshair_outline, 10)}
+        />
 
-    <Toggle
-      name="cl_crosshairdot"
-      label="Dot"
-      onClick={setConfigAction}
-      value={config.cl_crosshairdot}
-      isActive={config.cl_crosshairdot === '1'}
-    />
+        <Toggle
+          name="cl_crosshairdot"
+          label="Dot"
+          onClick={setConfigAction}
+          value={config.cl_crosshairdot}
+          isActive={config.cl_crosshairdot === '1'}
+        />
+      </div>
+      <div className="col-50">
+        <CrosshairColor
+          setConfigAction={setConfigAction}
+          activeColor={activeColor}
+        />
 
-    <CrosshairColor
-      setConfigAction={setConfigAction}
-      activeColor={activeColor}
-    />
+        <CustomColor
+          config={config}
+          onChange={setConfigAction}
+          disabled={activeColor !== '5'}
+        />
 
-    <CustomColor
-      config={config}
-      onChange={setConfigAction}
-      disabled={activeColor !== '5'}
-    />
-
-    <Presets
-      onClick={loadPresetAction}
-    />
+        <Presets
+          onClick={loadPresetAction}
+        />
+      </div>
+    </div>
 
     <CrosshairPreview config={config} />
 
