1fd909db011516e2cd04e783b744febb19534181
Achim Rolle <info@achimrolle.de>
2017-03-13 21:23:31 +0100

    move rect in canvas on click

--- src/components/CrosshairPreview/CrosshairPreview.js
+++ src/components/CrosshairPreview/CrosshairPreview.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import { Layer, Rect, Stage, Group } from 'react-konva';
 
 import { getPreviewColor } from '../../helpers/color';
@@ -9,23 +9,83 @@ type Props = {
   config: Object
 };
 
-const CrosshairPreview = ({ config } : Props) => (
-  <div className="crosshair-preview">
-    <Stage width={960} height={400}>
-      <Layer>
-        <Group
-          x={50}
-          y={50}
+type State = {
+  follow: boolean,
+  x: number,
+  y: number,
+};
+
+const canvasWidth = 960;
+const canvasHeight = 400;
+
+class CrosshairPreview extends Component {
+  props: Props;
+  state: State;
+
+  constructor(props: Props) {
+    super(props);
+
+    this.handleLayerClick = this.handleLayerClick.bind(this);
+    this.handleMousemove = this.handleMousemove.bind(this);
+
+    this.state = {
+      follow: false,
+      x: canvasWidth / 2,
+      y: canvasHeight / 2,
+    };
+  }
+
+  handleLayerClick() {
+    this.setState({
+      follow: !this.state.follow,
+    });
+  }
+
+  handleMousemove(e: Object) {
+    if (this.state.follow) {
+      // Thanks to: http://www.jacklmoore.com/notes/mouse-position/
+      const target = e.target || e.srcElement;
+      const rect = target.getBoundingClientRect();
+      const posX = e.clientX - rect.left;
+      const posY = e.clientY - rect.top;
+
+      this.setState({
+        x: posX,
+        y: posY,
+      });
+    }
+  }
+
+  render() {
+    const { config } = this.props;
+    const { x, y } = this.state;
+
+    return (
+      <div
+        className="crosshair-preview"
+        onClick={this.handleLayerClick}
+        onMouseMove={this.handleMousemove}
+      >
+        <Stage
+          width={canvasWidth}
+          height={canvasHeight}
         >
-          <Rect
-            width={10}
-            height={50}
-            fill={getPreviewColor(config)}
-          />
-        </Group>
-      </Layer>
-    </Stage>
-  </div>
-);
+          <Layer>
+            <Group
+              x={x}
+              y={y}
+            >
+              <Rect
+                width={10}
+                height={50}
+                fill={getPreviewColor(config)}
+              />
+            </Group>
+          </Layer>
+        </Stage>
+      </div>
+    );
+  }
+}
 
 export default CrosshairPreview;
