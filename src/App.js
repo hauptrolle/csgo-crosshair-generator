38eb8fc07adcccd0a962c953705765ccce10e60b
Achim Rolle <info@achimrolle.de>
2017-03-14 10:43:11 +0100

    update header design

--- src/App.js
+++ src/App.js
@@ -23,107 +23,119 @@ type Props = {
 
 const App = ({ config, setConfigAction, activeColor, loadPresetAction }: Props) => (
   <div className="app">
-    <h1>CS:GO Crosshair Generator</h1>
-    <div className="grid">
-      <div className="col-50">
-        <Toggle
-          name="cl_crosshairusealpha"
-          label="Use Alpha"
-          onClick={setConfigAction}
-          value={config.cl_crosshairusealpha}
-          isActive={config.cl_crosshairusealpha === '1'}
-        />
-
-        <Slider
-          disabled={config.cl_crosshairusealpha === '0'}
-          name="cl_crosshairalpha"
-          label="Alpha"
-          onChange={setConfigAction}
-          min={0}
-          max={255}
-          value={parseInt(config.cl_crosshairalpha, 10)}
-        />
-
-        <Slider
-          name="cl_crosshairthickness"
-          label="Thickness"
-          onChange={setConfigAction}
-          min={0}
-          max={100}
-          step={0.5}
-          value={parseInt(config.cl_crosshairthickness, 10)}
-        />
-
-        <Slider
-          name="cl_crosshairsize"
-          label="Size"
-          onChange={setConfigAction}
-          min={0}
-          max={100}
-          value={parseInt(config.cl_crosshairsize, 10)}
-        />
-
-        <Slider
-          name="cl_fixedcrosshairgap"
-          label="Gap"
-          onChange={setConfigAction}
-          min={-100}
-          max={100}
-          value={parseInt(config.cl_fixedcrosshairgap, 10)}
-        />
-
-        <Toggle
-          name="cl_crosshair_outline_draw"
-          label="Draw Outline"
-          onClick={setConfigAction}
-          value={config.cl_crosshair_outline_draw}
-          isActive={config.cl_crosshair_outline_draw === '1'}
-        />
-
-        <Slider
-          disabled={config.cl_crosshair_outline_draw === '0'}
-          name="cl_crosshair_outline"
-          label="Outline"
-          onChange={setConfigAction}
-          min={0}
-          max={3}
-          dots
-          value={parseInt(config.cl_crosshair_outline, 10)}
-        />
-
-        <Toggle
-          name="cl_crosshairdot"
-          label="Dot"
-          onClick={setConfigAction}
-          value={config.cl_crosshairdot}
-          isActive={config.cl_crosshairdot === '1'}
-        />
-      </div>
-      <div className="col-50">
-        <CrosshairColor
-          setConfigAction={setConfigAction}
-          activeColor={activeColor}
-        />
-
-        <CustomColor
-          config={config}
-          onChange={setConfigAction}
-          disabled={activeColor !== '5'}
-        />
-
-        <Presets
-          onClick={loadPresetAction}
-        />
+    <div className="header">
+      <div className="header-inner">
+        <div>
+          CS:GO Crosshair Generator
+        </div>
+        <button className="button" onClick={() => alert('Copy link to Clipboard')}>
+          Share Crosshair
+        </button>
       </div>
     </div>
 
-    <CrosshairPreview config={config} />
+    <div className="content">
+      <div className="grid">
+        <div className="col-50">
+          <Toggle
+            name="cl_crosshairusealpha"
+            label="Use Alpha"
+            onClick={setConfigAction}
+            value={config.cl_crosshairusealpha}
+            isActive={config.cl_crosshairusealpha === '1'}
+          />
 
-    {/* Debugging */}
-    <pre>
-      {JSON.stringify(config, null, 2)}
-    </pre>
-  </div>
+          <Slider
+            disabled={config.cl_crosshairusealpha === '0'}
+            name="cl_crosshairalpha"
+            label="Alpha"
+            onChange={setConfigAction}
+            min={0}
+            max={255}
+            value={parseInt(config.cl_crosshairalpha, 10)}
+          />
+
+          <Slider
+            name="cl_crosshairthickness"
+            label="Thickness"
+            onChange={setConfigAction}
+            min={0}
+            max={100}
+            step={0.5}
+            value={parseInt(config.cl_crosshairthickness, 10)}
+          />
+
+          <Slider
+            name="cl_crosshairsize"
+            label="Size"
+            onChange={setConfigAction}
+            min={0}
+            max={100}
+            value={parseInt(config.cl_crosshairsize, 10)}
+          />
+
+          <Slider
+            name="cl_fixedcrosshairgap"
+            label="Gap"
+            onChange={setConfigAction}
+            min={-100}
+            max={100}
+            value={parseInt(config.cl_fixedcrosshairgap, 10)}
+          />
+
+          <Toggle
+            name="cl_crosshair_outline_draw"
+            label="Draw Outline"
+            onClick={setConfigAction}
+            value={config.cl_crosshair_outline_draw}
+            isActive={config.cl_crosshair_outline_draw === '1'}
+          />
+
+          <Slider
+            disabled={config.cl_crosshair_outline_draw === '0'}
+            name="cl_crosshair_outline"
+            label="Outline"
+            onChange={setConfigAction}
+            min={0}
+            max={3}
+            dots
+            value={parseInt(config.cl_crosshair_outline, 10)}
+          />
+
+          <Toggle
+            name="cl_crosshairdot"
+            label="Dot"
+            onClick={setConfigAction}
+            value={config.cl_crosshairdot}
+            isActive={config.cl_crosshairdot === '1'}
+          />
+        </div>
+        <div className="col-50">
+          <CrosshairColor
+            setConfigAction={setConfigAction}
+            activeColor={activeColor}
+          />
+
+          <CustomColor
+            config={config}
+            onChange={setConfigAction}
+            disabled={activeColor !== '5'}
+          />
+
+          <Presets
+            onClick={loadPresetAction}
+          />
+        </div>
+      </div>
+
+      <CrosshairPreview config={config} />
+
+      {/* Debugging */}
+      <pre>
+        {JSON.stringify(config, null, 2)}
+      </pre>
+    </div>
+  </div >
 );
 
 const mapStateToProps = state => ({
