d613b912948c93c7e8b4c7149c3cad22d06d0cb7
Achim Rolle <info@achimrolle.de>
2017-03-16 17:32:33 +0100

    remove temp eslint-disable

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-/* eslint-disable */
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createHistory from 'history/createBrowserHistory';
 import { Route } from 'react-router-dom';
