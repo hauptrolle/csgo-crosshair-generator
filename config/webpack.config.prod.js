83ecfabace04e2e14614d9386b837b1a9114242e
Achim Rolle <info@achimrolle.de>
2017-03-25 12:08:40 +0100

    fix loader in webpack prod config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -142,7 +142,7 @@ module.exports = {
         test: /\.css$/,
         loader: ExtractTextPlugin.extract(
           'style',
-          loader: 'style!css?modules&importLoaders=1&localIdentName=[name]__[local]--[hash:base64:5]!postcss',
+          'style!css?modules&importLoaders=1&localIdentName=[name]__[local]--[hash:base64:5]!postcss',
           extractTextPluginOptions
         )
         // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
